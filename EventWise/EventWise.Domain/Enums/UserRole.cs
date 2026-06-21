a1b713ec7803839820a956742f3121d004bfdb7d
alasaneeth <alasaneeth22@gmail.com>
2026-06-21 13:16:22 +0530

    feat(domain): add UserRole enum

---
+++ EventWise/EventWise.Domain/Enums/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventWise.Domain.Enums;
+
+public enum UserRole
+{
+    Admin = 1,
+    EventOrganizer = 2,
+    FinanceManager = 3
+}
