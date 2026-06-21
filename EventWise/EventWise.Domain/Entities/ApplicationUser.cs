f103721381b5f144aba7294aa6be05dd9ecd8ff5
alasaneeth <alasaneeth22@gmail.com>
2026-06-21 13:18:17 +0530

    feat(domain): add ApplicationUser entity

---
+++ EventWise/EventWise.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace EventWise.Domain.Entities;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FullName { get; set; } = string.Empty;
+    public string? ProfilePicture { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; } = false;
+}
