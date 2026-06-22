71ee7a514571e1ef79f0455846387de93bb9c123
alasaneeth <alasaneeth22@gmail.com>
2026-06-22 15:05:33 +0300

    feat(application): add IAuthService interface

---
+++ EventWise/EventWise.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EventWise.Application.DTOs.Auth;
+
+namespace EventWise.Application.Interfaces;
+
+public interface IAuthService
+{
+    Task<AuthResponseDto> RegisterAsync(RegisterDto registerDto);
+    Task<AuthResponseDto> LoginAsync(LoginDto loginDto);
+    Task<AuthResponseDto> RefreshTokenAsync(string refreshToken);
+    Task<bool> LogoutAsync(string userId);
+}
