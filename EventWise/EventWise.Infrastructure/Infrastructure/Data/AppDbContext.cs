6cf784f464e23adeb3820d74df3c62b3541c4214
alasaneeth <alasaneeth22@gmail.com>
2026-06-22 15:10:48 +0300

    feat(infrastructure): add AppDbContext with Identity configuration

---
+++ EventWise/EventWise.Infrastructure/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EventWise.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventWise.Infrastructure.Data;
+
+public class AppDbContext : IdentityDbContext<ApplicationUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // Soft Delete Global Query Filter
+        builder.Entity<ApplicationUser>()
+            .HasQueryFilter(x => !x.IsDeleted);
+    }
+}
