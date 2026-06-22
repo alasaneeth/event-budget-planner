629a61ba085227d9940bb906c9b6945589d5f68f
alasaneeth <alasaneeth22@gmail.com>
2026-06-22 15:07:34 +0300

    feat(application): add RegisterValidator and LoginValidator

---
+++ EventWise/EventWise.Application/Validators/LoginValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EventWise.Application.DTOs.Auth;
+using FluentValidation;
+
+namespace EventWise.Application.Validators;
+
+public class LoginValidator : AbstractValidator<LoginDto>
+{
+    public LoginValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required");
+    }
+}
---
+++ EventWise/EventWise.Application/Validators/RegisterValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EventWise.Application.DTOs.Auth;
+using FluentValidation;
+
+namespace EventWise.Application.Validators;
+
+public class RegisterValidator : AbstractValidator<RegisterDto>
+{
+    public RegisterValidator()
+    {
+        RuleFor(x => x.FullName)
+            .NotEmpty().WithMessage("Full Name is required")
+            .MaximumLength(100).WithMessage("Full Name must not exceed 100 characters");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters")
+            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+            .Matches("[0-9]").WithMessage("Password must contain at least one number");
+
+        RuleFor(x => x.ConfirmPassword)
+            .Equal(x => x.Password).WithMessage("Passwords do not match");
+
+        RuleFor(x => x.Role)
+            .NotEmpty().WithMessage("Role is required");
+    }
+}
