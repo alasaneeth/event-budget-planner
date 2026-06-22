2a5f9a0fe512b05733cb2e6566d2999e429e0fdb
alasaneeth <alasaneeth22@gmail.com>
2026-06-22 15:03:53 +0300

    feat(application): add Auth DTOs

---
+++ EventWise/EventWise.Application/DTOs/Auth/AuthResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventWise.Application.DTOs.Auth;
+
+public class AuthResponseDto
+{
+    public string UserId { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
+    public DateTime TokenExpiry { get; set; }
+}
