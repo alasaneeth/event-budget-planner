519847d2eb757136ce9fe3452363c42737445600
alasaneeth <alasaneeth22@gmail.com>
2026-06-23 17:39:16 +0530

    test(auth): add unit tests for auth service login, logout, isLoggedIn and getRole

---
+++ EventWise/EventWise.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,40 @@
+﻿using EventWise.Application.Interfaces;
+using EventWise.Domain.Entities;
+using EventWise.Infrastructure.Data;
+using EventWise.Infrastructure.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventWise.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // DbContext
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection")));
+
+        // Identity
+        services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireNonAlphanumeric = false;
+        })
+        .AddEntityFrameworkStores<AppDbContext>()
+        .AddDefaultTokenProviders();
+
+        // Services
+        services.AddScoped<JwtService>();
+        services.AddScoped<IAuthService, AuthService>();
+
+        return services;
+    }
+}
